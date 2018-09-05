68a0740875c43e0cac7f49736942355733a822ae
Jeroen Van Dooren <jvandooren@gmmeise.local>
2018-09-05 11:11:19 +0200

    Update General
    General file has been updated to use functions.

--- General.ps1
+++ General.ps1
@@ -5,42 +5,60 @@
 ###                                        ###
 ###                                        ###
 ##############################################
+
+<#
+VARIABLES
+#>
 $USERNAME = $env:USERNAME
 $CREDENTIALPATH = "C:\Users\$USERNAME\Appdata\creds.txt"
-#$CREDENTIALFILE = "creds.txt"
+$CONNECTIONURI = "http://ACEXCVM001/Powershell/"
+
+<#
+IMPORT MODULES
+#>
+Import-Module ActiveDirectory
 
 
 <#
 GET ADMIN CREDENTIALS
 #>
-if(Test-Path $CREDENTIALPATH)
-{
-    $Password = Get-Content $CREDENTIALPATH | ConvertTo-SecureString 
+function GetAdminCredentials() {
+    if(Test-Path $CREDENTIALPATH) {
+        $Password = Get-Content $CREDENTIALPATH | ConvertTo-SecureString 
+    }
+    else {
+        $Password = Read-Host "Enter password from User $USERNAME" -AsSecureString | ConvertFrom-SecureString | Out-File $CREDENTIALPATH
+    }
 }
-else
-{
-    $Password = Read-Host "Enter password from User $USERNAME" -AsSecureString | ConvertFrom-SecureString | Out-File $CREDENTIALPATH
-}
-
 $Credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $USERNAME,$Password
 
 <#
 EXCHANGE CONNECTION
 #>
-Write-Host "Opening Exchange PSSession...."
-$ExchangeSession = New-PSSession -ConfigurationName Microsoft.EXCHANGE -ConnectionUri http://ACEXCVM001/Powershell/ -Authentication Kerberos -Credential $Credentials
-
-Import-PSSession $ExchangeSession -DisableNameChecking
-Write-Host "Exchange PSSession succesfully opened"
-
-<#
-IMPORT ACTIVE DIRECTORY
-#>
-Import-Module ActiveDirectory
+function MakeExchangeConnection() {
+    Write-Host "...Opening Exchange PSSession...."
+    GetAdminCredentials
+    try {
+        $ExchangeSession = New-PSSession -ConfigurationName Microsoft.EXCHANGE -ConnectionUri $CONNECTIONURI -Authentication Kerberos -Credential $Credentials
+        Import-PSSession $ExchangeSession -DisableNameChecking
+    }
+    catch {
+        "Error while opening Exchange PSSession!"
+    }
+    Write-Host "### Exchange PSSession succesfully opened ###`nYou can now use PS Exchange commands!"
+}
 
 <#
 VCENTER CONNECTION
 #>
-Write-Host "Opening VCenter Connection...."
-$VIConnection = Connect-VIServer "ACVCEVM001" -Credential $Credentials -WarningAction SilentlyContinue
-Write-Host "VIConnection can now be used to check VM Servers"
+function ConnectToVCenter(){
+    Write-Host "Opening VCenter Connection...."
+    GetAdminCredentials
+    try {
+        $VIConnection = Connect-VIServer "ACVCEVM001" -Credential $Credentials -WarningAction SilentlyContinue
+    }
+    Catch {
+        "Error while opening VCenter connection"
+    }
+    Write-Host "VIConnection can now be used to check VM Servers"
+}
--- Test.ps1
+++ Test.ps1
@@ -1,10 +1,2 @@
-$PASSWORDPATH = "C:\ProgramData\credentials.txt"
-$USER = $env:USERDOMAIN+"\"+$env:USERNAME
-
-if (!(Test-Path $PASSWORDPATH)) {
-    Read-Host "Geef uw paswoord op" -AsSecureString | ConvertFrom-SecureString | Out-File $PASSWORDPATH 
-}
-
-$password = Get-Content $Credentials | ConvertTo-SecureString
-$Credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $USER, $password
-
+. .\General.ps1
+MakeExchangeConnection
